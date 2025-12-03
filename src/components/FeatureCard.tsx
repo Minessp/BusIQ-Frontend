c6fb70e5e3a482915d7b4e29c391ed802d3f3ced
Minessp <minestadionchannel@gmail.com>
2025-12-03 20:57:25 -0300

    feat: Changed font style and fixed css bugs

--- src/components/FeatureCard.tsx
+++ src/components/FeatureCard.tsx
@@ -10,8 +10,8 @@ export default function FeatureCard ({ icon, title, description, isHovered } : F
 		<div className={`min-w-full min-h-[300px] flex flex-col items-center p-8 ${isHovered ? `bg-linear-to-b/oklch from-bg-light to-bg`
 			: `bg-linear-to-t from-bg from-85% to-bg-light`}`}>
 	  		<img src={icon} className="w-20 h-20 mb-4" alt={title} />
-	  		<h3 className="text-2xl font-semibold mb-6 text-text">{title}</h3>
-	  		<p className="text-center text-[18px] text-text-muted font-semibold">{description}</p>
+	  		<h3 className="text-2xl font-bold mb-6 text-text">{title}</h3>
+	  		<p className="text-center text-[18px] text-text-muted font-regular">{description}</p>
 		</div>
 	);
 };
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -4,7 +4,7 @@ import { features } from '../data/features';
 
 export default function Landing() {
 	return (
-		<body className='bg-linear-to-b from-bg-light via-bg to-bg-dark min-h-screen'>
+		<body className='font-inter bg-linear-to-b from-bg-light via-bg to-bg-dark min-h-screen'>
 			<header className='flex items-center justify-center pt-10 pb-2'>
 				<img src={Logo} alt="logo" className='w-[150px] h-fit' />
 			</header>
@@ -13,7 +13,7 @@ export default function Landing() {
 					<p className="mt-10 text-4xl font-bold tracking-tight text-text">
 						Você por dentro do transporte coletivo de São Paulo
 					</p>
-					<p className="mt-6 text-lg/8 font-semibold text-text-muted">
+					<p className="mt-6 text-lg/8 font-regular text-text-muted">
 						O Busiq é uma plataforma que oferece informações sobre o transporte coletivo na cidade de São Paulo. Com dados atualizados diariamente, você pode planejar suas rotas com mais eficiência e evitar atrasos.
 					</p>
 					<div className='mb-20'>
